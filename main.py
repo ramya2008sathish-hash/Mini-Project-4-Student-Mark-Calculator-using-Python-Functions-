e9de66c8d845678d10b32c5c25a03681e792a96d
ramya2008sathish-hash <ramya.2008sathish@gmail.com>
2026-06-24 15:36:21 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Function to calculate total def total_marks(m1, m2, m3):     return m1 + m2 + m3 
+ 
+# Function to calculate average def average_marks(total):     return total / 3 
+ 
+# Input marks m1 = int(input("Enter mark 1: ")) m2 = int(input("Enter mark 2: ")) m3 = int(input("Enter mark 3: ")) 
+ 
+# Function call total = total_marks(m1, m2, m3) avg = average_marks(total) 
+ 
+# Display result print("Total Marks:", total) print("Average Marks:", avg) 
